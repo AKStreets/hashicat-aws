d1bd9470ffa674fb2f94e53516e6c2eef297fc18
Your Name <you@example.com>
2021-03-19 22:23:39 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -117,8 +117,8 @@ resource "aws_eip_association" "hashicat" {
 
 resource "aws_instance" "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
-  billable                    = true
-  department                  = devops
+  Billable                    = true
+  Department                  = devops
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
